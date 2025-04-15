2b9c28bf357bea45d60ce68009acef8efdd08df5
parthshah1282 <parthshah1282@gmail.com>
2025-04-16 09:15:25 +1200

    Activity-1

---
+++ Activity-1.py
@@ -0,0 +1,7 @@
+import numpy as np
+array = np.arange(1,11)
+print("Original Array:",array)
+print("shape:",array.shape)
+print("Data Type:",array.dtype)
+Double_Array = array * 2
+print("Multiply:",Double_Array)
